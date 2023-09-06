f657841651b931383bcdff1a411b947959b9a835
Jessika <miss_zest@hotmail.com>
2023-09-06 15:25:44 -0400

    code source

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import jess from './jess.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>C'est un test Git-Hub Avec React</h1>
+      <img src={jess} width="550px" alt='photo' />
+     </div>
+  );
+}
+
+export default App;
